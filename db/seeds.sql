66eed5442a229977c54cd051a39dcfa94d0d1ab3
Taylor Walker <taylor.walker@hotmail.com>
2018-12-20 16:50:23 -0800

    controller/handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Steer Burger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Infamous Star', true);
+INSERT INTO burgers (name, devoured) VALUES ('Rabbit Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Nothing Burger', true);
+INSERT INTO burgers (name) VALUES ('Kitchen Sink Burger');
